d8e9da7050ebddd5cec52aa9656a26fc080b54a9
1F10230183 <s1F102301831@iniad.org>
2023-05-02 00:17:29 +0900

    裏返せるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,25 +14,48 @@ const Home = () => {
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
   ]);
-  const numList = [
+  const directionList = [
     [1, 0],
     [-1, 0],
-    [1, 0],
-    [-1, 0],
+    [0, 1],
+    [0, -1],
     [-1, -1],
     [1, 1],
     [1, -1],
     [-1, 1],
   ];
+
   const clickCell = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = JSON.parse(JSON.stringify(board));
-    for (const n of numList)
-      if (board[y + n[0]] !== undefined && board[y + n[0]][x + n[1]] === 3 - turnColor) {
-        newBoard[y][x] = turnColor;
-        setTurnColor(3 - turnColor);
+    const changeStonesList = [];
+    for (const n of directionList) {
+      console.log(n);
+      for (let space = 1; space < 8; space += 1) {
+        console.log(space);
+        console.table(changeStonesList);
+        console.log(turnColor === 1 ? 'kuro' : 'shiro');
+        console.table(newBoard);
+        if (board[y + n[0] * space] === undefined || board[x + n[1] * space] === undefined) {
+          console.log('a');
+          break;
+        } else if (board[y + n[0] * space][x + n[1] * space] === 0) {
+          console.log('b');
+          break;
+        } else if (board[y + n[0] * space][x + n[1] * space] === 3 - turnColor) {
+          console.log('c');
+          changeStonesList.push([y + n[0] * space, x + n[1] * space]);
+        } else if (space >= 2 && board[y + n[0] * space][x + n[1] * space] === turnColor) {
+          console.log('d');
+          for (const m of changeStonesList) {
+            newBoard[m[0]][m[1]] = turnColor;
+          }
+          newBoard[y][x] = turnColor;
+          break;
+        }
       }
-
+    }
+    setTurnColor(3 - turnColor);
     setBoard(newBoard);
   };
   return (
