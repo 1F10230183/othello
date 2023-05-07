d394ed8feb50d4ffd9cb158f6ecb86fff2fdfcc4
1F10230183 <1F10230183@users.noreply.github.com>
2023-05-07 13:16:46 +0000

    deploy: cd1dc45e454fbd0f1463480e7524cf50416feb79

---
+++ _next/static/HxKlG5NvLIg4RxKWm3Fh1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b1a375fa1945d730.css","static/chunks/pages/index-d942399cef67b330.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
